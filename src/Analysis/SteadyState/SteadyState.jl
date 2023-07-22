592b2fe4a228f47d801614062ea61b284deef1a9
BaselMorsy <s-basel.abdelazim@zewailcity.edu.eg>
2023-07-22 13:12:23 +0200

    Adding innovations

--- src/Analysis/SteadyState/SteadyState.jl
+++ src/Analysis/SteadyState/SteadyState.jl
@@ -1,2 +1,4 @@
 include("DeterministicAnalysis/DOPF.jl")
+include("DeterministicAnalysis/Innovations/CCG_DOPF.jl")
+include("DeterministicAnalysis/Innovations/Grid_Chains.jl")
 include("PowerFlow.jl")
