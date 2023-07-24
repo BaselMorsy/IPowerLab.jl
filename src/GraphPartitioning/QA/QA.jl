900b33aaaf21a60258ecb0183b861bf3264fa4a1
hjaleta <hjaleta@gmail.com>
2023-07-24 22:48:51 +0200

    add some explanatory comments for setup

--- src/GraphPartitioning/QA/QA.jl
+++ src/GraphPartitioning/QA/QA.jl
@@ -1,3 +1,14 @@
+# In order to run this file, you need to follow a few steps:
+
+# 1. Install the Julia package PyCall. When adding PyCall, it will by default use a miniconda environment
+# You need to install dwave-ocean-sdk into that environment
+
+# 2. (Only necessary if you need the clous services) Create an account on D-waves  website and get an API key
+# You should then save the api key in a file named api_key.json in the same directory as this file. The file should have the following format:
+# {
+#     "api_key": "<your api key>",
+# }
+
 using PyCall
 
 @pyinclude("src/GraphPartitioning/QA/dwave_call.py")
