9e21fe0d2b2efe34e2794851e880fc4652d9618f
BaselMorsy <s-basel.abdelazim@zewailcity.edu.eg>
2023-07-25 16:49:00 +0200

    directory change

--- src/Analysis/SteadyState/DeterministicAnalysis/Innovations/GraphPartitioning/QA/QA.jl
+++ src/Analysis/SteadyState/DeterministicAnalysis/Innovations/GraphPartitioning/QA/QA.jl
@@ -11,10 +11,10 @@
 
 using PyCall
 
-@pyinclude("src/GraphPartitioning/QA/dwave_call.py")
+@pyinclude("src/Analysis/SteadyState/DeterministicAnalysis/Innovations/GraphPartitioning/QA/dwave_call.py")
 
-sample_qubo = pyimport("src.GraphPartitioning.QA.dwave_call").sample_qubo
+sample_qubo = pyimport("src.Analysis.SteadyState.DeterministicAnalysis.Innovations.GraphPartitioning.QA.dwave_call").sample_qubo
 
 qubo_matrix = [[-1 0 3]; [0 2 1]; [ 0 0 -0.5]]
 
-sample_qubo(qubo_matrix)
+sample_qubo(qubo_matrix, solver="simulated_annealing")
