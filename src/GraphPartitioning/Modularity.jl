fa61cc3c77d7b81bdea21ed557d6f360f49e834e
hjaleta <hjaleta@gmail.com>
2023-07-26 13:16:24 +0200

    modulaity function

---
+++ src/GraphPartitioning/Modularity.jl
@@ -0,0 +1,11 @@
+
+function buildModularityQubo(adjacency_matrix::AbstractMatrix, num_communities::Int64)
+    # Build the QUBO matrix for the modularity problem
+    # adjacency_matrix: adjacency matrix of the graph
+    # num_communities: number of communities to partition the graph into
+    # returns: QUBO matrix
+    return modularityQubo
+end
+
+    
+
